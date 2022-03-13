86898fae07f813e1ee56bd2802c2e620772d22db
anonymous anonymous <noelle.harasym@gmail.com>
2022-03-13 19:06:44 +0000

    Direct association between users and coffee_orders

--- app/models/coffee_order.rb
+++ app/models/coffee_order.rb
@@ -1,6 +1,8 @@
 class CoffeeOrder < ApplicationRecord
   # Direct associations
 
+  belongs_to :user
+
   # Indirect associations
 
   # Validations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :coffee_orders,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
