73d3e80130048993e6af2784079371e4dce7010e
anonymous anonymous <noelle.harasym@gmail.com>
2022-03-13 19:06:57 +0000

    Whitelist columns in CoffeeType for ActiveAdmin

--- app/admin/coffee_types.rb
+++ app/admin/coffee_types.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register CoffeeType do
 
+ permit_params :coffee_type
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
