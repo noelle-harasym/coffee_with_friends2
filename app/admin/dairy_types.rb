9ae694fb6290f924173a7c7ef6cdbd7014c9778d
anonymous anonymous <noelle.harasym@gmail.com>
2022-03-13 19:06:59 +0000

    Whitelist columns in DairyType for ActiveAdmin

--- app/admin/dairy_types.rb
+++ app/admin/dairy_types.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register DairyType do
 
+ permit_params :dairy_type
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
