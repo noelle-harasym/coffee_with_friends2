b98e8a36160eea44144ba99c1ef81c46604da4cd
anonymous anonymous <noelle.harasym@gmail.com>
2022-03-13 19:06:55 +0000

    Whitelist columns in CoffeeOrder for ActiveAdmin

--- app/admin/coffee_orders.rb
+++ app/admin/coffee_orders.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register CoffeeOrder do
 
+ permit_params :coffe_type_id, :dairy_type_id, :iced, :modifications_other, :rating, :picture, :date, :user_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
